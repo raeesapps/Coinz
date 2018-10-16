9ef83afc05183d01231c6906da776642062434b3
raeesapps <raees.aamir@yandex.com>
2018-10-16 18:49:46 +0100

    Menu screen UI completed

---
+++ Implementation/app/src/main/java/net/raeesaamir/coinz/MenuController.java
@@ -0,0 +1,39 @@
+package net.raeesaamir.coinz;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * MVC controller for the Menu.
+ * @author raeesaamir
+ */
+public class MenuController extends AppCompatActivity {
+
+    /**
+     * The different options in the menu
+     */
+    private static final List<String> MENU_OPTIONS = Lists.newArrayList("Play", "Wallet", "Trade", "Leaderboard", "About");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_view);
+        populateListView();
+    }
+
+    /**
+     * Populates the list UI with the menu options
+     */
+    private void populateListView() {
+        ListView menu = findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, MENU_OPTIONS);
+        menu.setAdapter(adapter);
+    }
+}
--- Implementation/app/src/main/java/net/raeesaamir/coinz/SplashScreenController.java
+++ Implementation/app/src/main/java/net/raeesaamir/coinz/SplashScreenController.java
@@ -1,13 +1,28 @@
 package net.raeesaamir.coinz;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenController extends AppCompatActivity {
 
+    private static final int SPLASH_SCREEN_DURATION = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_view);
+
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent menu = new Intent(getApplicationContext(), MenuController.class);
+                startActivity(menu);
+                handler.removeCallbacks(this);
+            }
+        }, SPLASH_SCREEN_DURATION);
     }
 }
